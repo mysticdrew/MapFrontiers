a82030aea8c8f202fbdd3521f687868e4a5f76f0
Alejandro Coria <alejandrocoria2009@gmail.com>
2024-11-24 18:30:06 -0300

    updated dependencies

--- Fabric/src/main/java/games/alejandrocoria/mapfrontiers/client/modmenu/ModMenuApiImpl.java
+++ Fabric/src/main/java/games/alejandrocoria/mapfrontiers/client/modmenu/ModMenuApiImpl.java
@@ -2,19 +2,12 @@ package games.alejandrocoria.mapfrontiers.client.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import com.terraformersmc.modmenu.gui.ModsScreen;
 import games.alejandrocoria.mapfrontiers.client.gui.screen.ModSettings;
 
 public class ModMenuApiImpl implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return s -> {
-            if (s instanceof ModsScreen) {
-                boolean hasConfigScreen = ((ModsScreen) s).getModHasConfigScreen().getOrDefault("mapfrontiers", false);
-                if (!hasConfigScreen) {
-                    return ModSettings.createDummy();
-                }
-            }
             ModSettings screen = new ModSettings(true);
             screen.display();
             return screen;
